19a58c004134a3076b538976837364c8e002029e
agnosus <gahkani@gmail.com>
2024-01-28 22:01:39 +0100

    Remove Badge component and link

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -9,59 +9,8 @@ type Props = {
 const defaultTextColor = '#303235';
 
 export const Badge = (props: Props) => {
-  let liteBadge: HTMLAnchorElement | undefined;
-  let observer: MutationObserver | undefined;
+  // Remove all the code related to the badge and the link
 
-  const appendBadgeIfNecessary = (mutations: MutationRecord[]) => {
-    mutations.forEach((mutation) => {
-      mutation.removedNodes.forEach((removedNode) => {
-        if ('id' in removedNode && liteBadge && removedNode.id == 'lite-badge') {
-          console.log("Sorry, you can't remove the brand 😅");
-          props.botContainer?.append(liteBadge);
-        }
-      });
-    });
-  };
-
-  onMount(() => {
-    if (!document || !props.botContainer) return;
-    observer = new MutationObserver(appendBadgeIfNecessary);
-    observer.observe(props.botContainer, {
-      subtree: false,
-      childList: true,
-    });
-  });
-
-  onCleanup(() => {
-    if (observer) observer.disconnect();
-  });
-
-  return (
-    <span
-      style={{
-        'font-size': '13px',
-        position: 'absolute',
-        bottom: 0,
-        padding: '10px',
-        margin: 'auto',
-        width: '100%',
-        'text-align': 'center',
-        color: props.poweredByTextColor ?? defaultTextColor,
-        'background-color': props.badgeBackgroundColor ?? '#ffffff',
-      }}
-    >
-      Powered by
-      <a
-        ref={liteBadge}
-        href={'https://flowiseai.com'}
-        target="_blank"
-        rel="noopener noreferrer"
-        class="lite-badge"
-        id="lite-badge"
-        style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
-      >
-        <span> Chatfleet.ai</span>
-      </a>
-    </span>
-  );
+  // Return an empty fragment
+  return <></>;
 };
